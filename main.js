08cdeadbc9057595ca057722306c51fcdb6ebfca
needhi-propGoto <m.needhichozhan@propertyautomate.com>
2025-01-17 12:10:43 +0530

    changes in query

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ connection.connect().then(() => {
     console.log('Error connecting to database', err);
 });
 
-connection.query('SELECT country, COUNT(*) FROM person GROUP BY country', (err, res) => {
+connection.query('SELECT country, COUNT(*) FROM person GROUP BY country FETCH FIRST 10 ROW ONLY', (err, res) => {
     if (err) {
         console.error(err);
         return;
