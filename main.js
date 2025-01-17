abb31b85270df12fe7ebd6040abdef3d71c7c8e9
needhi-propGoto <m.needhichozhan@propertyautomate.com>
2025-01-17 12:05:51 +0530

    init commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const { Client } = require('pg');
+
+
+const connection  = new Client({
+    host: 'localhost',
+    user: 'postgres',
+    port: 5432,
+    password: '',
+    database: 'test'
+});
+
+connection.connect().then(() => {
+    console.log('Connected to database');
+} ).catch((err) => {    
+    console.log('Error connecting to database', err);
+});
+
+connection.query('SELECT country, COUNT(*) FROM person GROUP BY country', (err, res) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(res.rows);
+    connection.end();
+});
